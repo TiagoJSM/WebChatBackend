00993c9f6029a2fd9fa3ea26147701c1d2bc7b59
Tiago Martins <tiago_gobackto80s@hotmail.com>
2021-06-30 09:05:45 +0200

    Add css file serve. Change homepage to be a static page instead of a template page.

--- main.go
+++ main.go
@@ -2,36 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"os"
-	"text/template"
 
 	"github.com/TiagoJSM/WebChatBackend/messages"
 	"github.com/labstack/echo/v4"
 )
 
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
 func main() {
 	messagesRepository := messages.NewMessagesRepository()
 	messagesController := messages.NewMessagesController(messagesRepository)
 
-	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
-	}
-
 	e := echo.New()
-	e.Renderer = t
-	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "homepage.html", "")
-	})
+	e.Static("/static", "assets")
+	e.File("/", "public/index.html")
 	e.GET("/messages", messagesController.GetAll)
 	e.POST("/message", messagesController.PostMessage)
 	e.GET("/ws", messagesController.ConnectToSocket)
